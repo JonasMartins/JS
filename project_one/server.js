a0bc6c51c72b026e5506af435ee5e94dcf4bb9ea
JonasMartins <jonas.w.martins@gmail.com>
2018-08-16 14:22:54 -0300

    project_one 192

--- project_one/server.js
+++ project_one/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 const app = express();
 
 // database configuration
 const db = require('./config/keys').mongoURI;
 
 // connecting with mongoDB
-mongoose.connect(db)
-.then(
-  () => console.log('Sucessfully connected')
-).catch(err => console.log(err));
+// mongoose.connect(db, { useNewUrlParser: true })
+// .then(
+//   () => console.log('Sucessfully connected')
+// ).catch(err => console.log(err));
 
 app.get('/',(req,res) => res.send('hello world'));
+
+app.use('/api/users',users); // go to const with same name above
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
 
 const port = process.env.PORT || 3000;
 
